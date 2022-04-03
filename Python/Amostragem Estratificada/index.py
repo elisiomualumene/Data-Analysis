c34692272762eb30e0c2d60bcf34596123979596
elisiomualumene <elisiomualumene@gmail.com>
2022-04-03 20:10:58 +0100

    train_test_split

--- Python/Amostragem Estratificada/index.py
+++ Python/Amostragem Estratificada/index.py
@@ -1,7 +1,12 @@
 # Amostragem Estratificada
 
 import pandas as pd
-# from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
+
 iris = pd.read_csv('iris.csv')
+print(iris)
 data = iris['variety'].value_counts()
 print(data)
+
+#x, _, y, _ = train_test_split(iris.iloc[:, 0:3], iris.iloc[:, 4], test_size=0.5, stratify=iris.iloc[:, 4])
+print(train_test_split(iris.iloc[:, 0:3], iris.iloc[:, 4], test_size=0.5, stratify=iris.iloc[:, 4]))
