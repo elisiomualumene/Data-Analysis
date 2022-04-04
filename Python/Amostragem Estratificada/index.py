b946eb19d5006116f396aaba2906a404a527e896
elisiomualumene <elisiomualumene@gmail.com>
2022-04-04 08:49:07 +0100

    :zap:add infert csv

--- Python/Amostragem Estratificada/index.py
+++ Python/Amostragem Estratificada/index.py
@@ -4,9 +4,13 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 iris = pd.read_csv('iris.csv')
-print(iris)
 data = iris['variety'].value_counts()
-print(data)
 
-#x, _, y, _ = train_test_split(iris.iloc[:, 0:3], iris.iloc[:, 4], test_size=0.5, stratify=iris.iloc[:, 4])
-print(train_test_split(iris.iloc[:, 0:3], iris.iloc[:, 4], test_size=0.5, stratify=iris.iloc[:, 4]))
+x, _, y, _ = train_test_split(iris.iloc[:, 0:4], iris.iloc[:, 4], test_size=0.5, stratify=iris.iloc[:, 4])
+print(y.value_counts())
+
+infert = pd.read_csv('infert.csv')
+split = infert['education'].value_counts()
+print(split)
+
+x1, _, y1, _ = train_test_split(infert.iloc[:, 2:9], infert.iloc[:, 1], test_size=0.6, stratify=infert.iloc[:, 1])
